0fa7df9cc3120766d0cd6bdf3cb13d6ba6adf910
Daffyta <daffy.dine9@gmail.com>
2017-06-12 10:07:07 -0500

    metodo get

--- js/main.js
+++ js/main.js
@@ -4,25 +4,14 @@ var cargarPagina = function() {
 };
 
 var cargarPersonajes = function() {
-  // este es un objeto que permite obtener la información que contiene el api de star wars
-  // la url como parámetro de tipo string por eso va entre comillas
-  $.ajax("http://swapi.co/api/people/", {
-    // estas son las minimas propiedades que hay que usar
-    method : "GET",
-    dataType : "json",
-  // success y error son dos métodos
-    success: function(response) {
-      // del objeto que nos arroja el api, cuya propiedad results tiene un arreglo de objetos
-      var personajes = response.results;
-      var total = response.count;
-      mostrarTotalPersonajes(total);
-      mostrarPersonajes(personajes);
-
-    },
-    error: function(error) {
-      console.log("error",error);
-    }
+  var url = "http://swapi.co/api/people/";
+  $.get( url, function (response) {
+    var personajes = response.results;
+    var total = response.count;
+    mostrarTotalPersonajes(total);
+    mostrarPersonajes(personajes);
   });
+
 };
 
 var mostrarTotalPersonajes = function (total) {
